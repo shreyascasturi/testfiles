147d8b80ca05e5e812cfe570fb2fd93cc5ccd4b1
Shreyas Casturi <shreyascasturi@gmail.com>
2019-08-10 13:57:38 -0400

    changed init.el file for DejaVu Sans Mono-10

--- init.el
+++ init.el
@@ -173,5 +173,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#1D252C" :foreground "#A0B3C5" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 113 :width normal :foundry "PfEd" :family "Iosevka Regular")))))
+ '(default ((t (:inherit nil :stipple nil :background "#1D252C" :foreground "#A0B3C5" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 98 :width normal :foundry "PfEd" :family "DejaVu Sans Mono")))))
 (put 'narrow-to-page 'disabled nil)
